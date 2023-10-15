0ecd1177c2089caf382feaba8c2bc59fb0a28890
Jaip3r <113143589+Jaip3r@users.noreply.github.com>
2023-10-15 13:37:53 -0500

    Update allowedOrigins.js
    Configuración de CORS

--- src/util/allowedOrigins.js
+++ src/util/allowedOrigins.js
@@ -3,5 +3,5 @@ export const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:3500',
     'https://luc4shk.github.io',
-    'https://cheerful-strudel-8a37c7.netlify.app/'
+    'https://cheerful-strudel-8a37c7.netlify.app'
 ];
