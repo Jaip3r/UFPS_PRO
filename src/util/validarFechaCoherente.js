1060c2576a21f77eb059c7156e903d467f3cd56c
Jaip3r <jaidergustavoolmo@ufps.edu.co>
2023-10-25 19:45:59 -0500

    Probando fechas convocatoria 2

--- src/util/validarFechaCoherente.js
+++ src/util/validarFechaCoherente.js
@@ -14,25 +14,24 @@ export function validarFechaCoherente(fecha_inicio, fecha_fin) {
 
     // Fechas a comparar
     const fechaActual = moment().tz('America/Bogota');
-    const fecha_inicio_format = moment(fecha_inicio, format);
-    const fecha_fin_format = moment(fecha_fin, format);
-    console.log('Fechas servidor');
-    console.log(fechaActual, fecha_inicio_format, fecha_fin_format);
+    const fecha_inicio_format = moment(fecha_inicio, format).tz('America/Bogota');
+    const fecha_fin_format = moment(fecha_fin, format).tz('America/Bogota');
 
-    const fecha_inicio_col = moment(fecha_inicio, format).tz('America/Bogota');
-    const fecha_fin_col = moment(fecha_fin, format).tz('America/Bogota');
+    const fecha_inicio_col = fecha_inicio_format.add(5, 'hours');
+    const fecha_fin_col = fecha_fin_format.add(5, 'hours');
+
     console.log('Fechas colombia');
     console.log(fechaActual, fecha_inicio_col, fecha_fin_col);
 
-    if (fecha_inicio_format.isBefore(fechaActual)) {
+    if (fecha_inicio_col.isBefore(fechaActual)) {
         return 'La fecha de inicio de la convocatoria no es coherente';
     }
 
-    if(fecha_fin_format.isBefore(fechaActual)) {
+    if(fecha_fin_col.isBefore(fechaActual)) {
         return 'La fecha de fin de la convocatoria no es coherente';
     }
 
-    if(fecha_inicio_format.isSameOrAfter(fecha_fin_format)) {
+    if(fecha_inicio_col.isSameOrAfter(fecha_fin_col)) {
         return 'La fecha de inicio de la convocatoria no puede ser mayor o igual que la de fin';
     }
   
